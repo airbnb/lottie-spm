7b7aaeb26e03397b01f8ff386f947634200ee6e6
Cal Stephens <cal.stephens@airbnb.com>
2023-01-20 08:29:46 -0800

    Fix issue where framework couldn't be embedded in app product (#5)

--- Package.swift
+++ Package.swift
@@ -5,12 +5,20 @@ import PackageDescription
 let package = Package(
   name: "Lottie",
   platforms: [.iOS("11.0"), .macOS("10.10"), .tvOS("11.0")],
-  products: [.library(name: "Lottie", targets: ["Lottie"])],
+  products: [.library(name: "Lottie", targets: ["Lottie", "_LottieStub"])],
   targets: [
     .binaryTarget(
       name: "Lottie",
       url: "https://github.com/airbnb/lottie-ios/releases/download/4.1.0/Lottie.xcframework.zip",
       checksum: "e954f5bfc5049196593c59c6eb21321016ffaba4417e9b4ece2a6a9eb54baa9a"),
+    
+    // Without at least one regular (non-binary) target, this package doesn't show up
+    // in Xcode under "Frameworks, Libraries, and Embedded Content". That prevents
+    // Lottie from being embedded in the app product, causing the app to crash when
+    // ran on a physical device. As a workaround, we can include a stub target
+    // with at least one source file.
+    .target(name: "_LottieStub"),
+    
     .testTarget(
       name: "LottieTests",
       dependencies: ["Lottie"],
